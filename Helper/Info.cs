54e4b8324fe1170085512f5dfff686012314fce6
Muhammad <psdesigner@hotmail.com>
2015-12-03 03:31:13 -0800

    Add SystemInfo class

---
+++ Helper/Info.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helper
+{
+    public class OperatingSystem
+    {
+
+    }
+    public static class SysyemInfo
+    {
+        public static List<Helper.OperatingSystem> get()
+        {
+            return new List<OperatingSystem>();
+        }
+
+    }
+}
