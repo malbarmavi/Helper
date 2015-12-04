fcfa3eaa51f82cd8c3b560edd7ab84fdd6ed83f1
Muhammad <psdesigner@hotmail.com>
2015-12-04 07:01:18 -0800

    Working on info methods

--- Helper/Info.cs
+++ Helper/Info.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Management;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +9,39 @@ namespace Helper
 {
     public class OperatingSystem
     {
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public string Architecture { get; set; }
+        public DateTime InstallDate { get; set; }
+        public DateTime LastRestart { get; set; }
+        public string ProductType { get; set; }
+        public string PrimaryOS { get; set; }
+        public string SerialNumber { get; set; }
+        public string RegisteredUser { get; set; }
+        public string Organization { get; set; }
+        public string BootDevice { get; set; }
+        public string SystemDevice { get; set; }
+        public string SystemDrive { get; set; }
+        public string WindowsDirectory { get; set; }
+        public string SystemDirectory { get; set; }
+
 
     }
     public static class SysyemInfo
     {
-        public static List<Helper.OperatingSystem> get()
+        public static List<OperatingSystem> GetOperatingSystems()
         {
-            return new List<OperatingSystem>();
+            var result = new List<OperatingSystem>();
+            ManagementObjectSearcher managmentSearcher = new ManagementObjectSearcher("select * from Win32_Account");
+
+            foreach (ManagementObject managmentObj in managmentSearcher.Get())
+            {
+                var os = new OperatingSystem();
+                // get the data
+                result.Add(os);
+
+            }
+            return result;
         }
 
     }
