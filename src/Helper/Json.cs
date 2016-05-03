778a06842a4d37c082460ed07a214e38b8848457
mbarmawi <muhammadbarmawi@outlook.com>
2016-05-02 22:53:23 -0800

    Update files structural

---
+++ src/Helper/Json.cs
@@ -0,0 +1,11 @@
+﻿using System.Web.Script.Serialization;
+
+namespace Helper
+{
+  public static class Json
+  {
+    public static string Stringify(object obj) => new JavaScriptSerializer().Serialize(obj);
+
+    public static T parse<T>(string obj) => new JavaScriptSerializer().Deserialize<T>(obj);
+  }
+}
