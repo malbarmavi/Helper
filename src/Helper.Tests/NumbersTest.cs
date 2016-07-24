82728ff2532f123e2d8e9be09222b64f22fe48c7
mbarmawi <muhammadbarmawi@outlook.com>
2016-07-23 18:34:33 -0800

    Improve unit testing

---
+++ src/Helper.Tests/NumbersTest.cs
@@ -0,0 +1,63 @@
+﻿using Xunit;
+namespace Helper.Tests
+{
+
+  public class NumbersTest
+  {
+    [Fact]
+    public void ToInt_Given0_shouldReturn0()
+    {
+      // arrange
+      int num = 0;
+
+      // act
+      int result = Numbers.Map<int>(0);
+
+      // assert
+      Assert.Equal(num, result);
+    }
+
+    [Fact]
+    public void ToInt_GivenDoubleValue_shouldReturn1()
+    {
+      // arrange
+      double num = 1;
+
+      // act
+      int result = Numbers.Map<int>(1.2);
+
+      // assert
+      Assert.Equal(num, result);
+    }
+
+    [Fact]
+    public void ToInt_GivenDoubleValue_shouldReturn2()
+    {
+      // arrange
+      double num = 2;
+
+      // act
+      int result = Numbers.Map<int>(1.5);
+
+      // assert
+      Assert.Equal(num, result);
+    }
+
+
+    [Fact]
+    public void ToInt_GivenNull_shouldReturn0()
+    {
+      // arrange
+      double num = 0;
+
+      // act
+      int result = Numbers.Map<int>(null);
+
+      // assert
+      Assert.Equal(num, result);
+    }
+
+
+
+  }
+}
