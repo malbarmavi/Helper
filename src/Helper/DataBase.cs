ba4951a527fcfab2272fdf0517f97927799b095a
mbarmawi <muhammadbarmawi@outlook.com>
2016-09-09 11:41:38 -0800

    Add documentation to database methods

--- src/Helper/DataBase.cs
+++ src/Helper/DataBase.cs
@@ -7,6 +7,11 @@ namespace Helper
 {
   public static class DataBase
   {
+    /// <summary>
+    /// Test is the connection string valid by establish the connection
+    /// </summary>
+    /// <param name="connectionString"></param>
+    /// <returns></returns>
     public static bool TestConnectionString(string connectionString)
     {
       try
@@ -23,6 +28,12 @@ namespace Helper
       }
     }
 
+    /// <summary>
+    /// Get data stored in DataTable objet 
+    /// </summary>
+    /// <param name="sqlStatement">sql query</param>
+    /// <param name="connectionString">sqlserer connection string</param>
+    /// <returns></returns>
     public static Result<DataTable> GetData(string sqlStatement, string connectionString)
     {
       var result = new Result<DataTable>();
@@ -44,7 +55,12 @@ namespace Helper
         return result;
       }
     }
-
+    /// <summary>
+    /// Execute ommand like insert,delete...
+    /// </summary>
+    /// <param name="sqlStatement">sql query</param>
+    /// <param name="connectionString">sqlserer connection string</param>
+    /// <returns></returns>
     public static Result<object> ExecuteNonQuery(string sqlStatement, string connectionString)
     {
       try
@@ -71,7 +87,12 @@ namespace Helper
         };
       }
     }
-
+    /// <summary>
+    /// Execute sql statement return single value like count,sum...
+    /// </summary>
+    /// <param name="sqlStatement">sql query</param>
+    /// <param name="connectionString">sqlserer connection string</param>
+    /// <returns></returns>
     public static Result<object> ExecuteScalar(string sqlStatement, string connectionString)
     {
       var result = new Result<object>();
--- src/Helper/Json.cs
+++ src/Helper/Json.cs
@@ -4,8 +4,19 @@ namespace Helper
 {
   public static class Json
   {
+    /// <summary>
+    /// Converts a object to a JSON string
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns></returns>
     public static string Stringify(object obj) => new JavaScriptSerializer().Serialize(obj);
 
+    /// <summary>
+    ///  Parses a JSON string into T type object
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="obj"></param>
+    /// <returns></returns>
     public static T Parse<T>(string obj) => new JavaScriptSerializer().Deserialize<T>(obj);
   }
 }
