e9dffecd677ee535db086b17d514b4d58b56e5ad
mbarmawi <muhammadbarmawi@outlook.com>
2016-09-12 00:24:39 -0800

    Add http class

---
+++ src/Helper/Http.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helper
+{
+  public static class Http
+  {
+    public static async Task<string> DownloadString(string url)
+    {
+      WebClient wc = GetWebClient();
+      return await wc.DownloadStringTaskAsync(url);
+    }
+
+    public static async Task<string> Get(string url)
+    {
+      WebClient wc = GetWebClient();
+      wc.Headers[HttpRequestHeader.ContentType] = "application/json";
+      return await wc.DownloadStringTaskAsync(url);
+    }
+
+    public static async Task Post(string url)
+    {
+      throw new NotImplementedException();
+    }
+
+
+    private static WebClient GetWebClient()
+    {
+      WebClient wc = new WebClient();
+      wc.Encoding = Encoding.UTF8;
+      return wc;
+    }
+  }
+}
